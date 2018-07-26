1a15bb304459434292e7d511b79c1bfad39ef031
Mallika Sinha <msinha034@gmail.com>
2018-07-26 22:38:32 +0530

    laugh.js updated

--- utilites/fb_crawler.js
+++ utilites/fb_crawler.js
@@ -1,10 +1,9 @@
 let startUrls = {
-    // "Sadcasm": "https://www.facebook.com/Sadcasm/photos/a.458106047914642.1073741828.458104154581498/918639761861266/?type=3&theater"
-   "Laugh": "https//www.facebook.com/laughingcolours/photos/a.10150471189319578.388832.173770089577/10157632423039578/?type=3&theater"
+    //"Sadcasm": "https://www.facebook.com/Sadcasm/photos/a.458106047914642.1073741828.458104154581498/918639761861266/?type=3&theater"
+    "Laugh": "https://www.facebook.com/laughingcolours/photos/a.10150471189319578.388832.173770089577/10156779268339578/?type=3&theater"
 };
 
 let images=[];
-
 const isDuplicate = (img) =>{
     return images.length > 0 && images[images.length - 1][0] === img && img !== null;
 };
@@ -42,7 +41,6 @@ setInterval(function(){
   once it displays  a msg - reached image threshold,   then type JSON.stringify(images) 
   then click on copy  and paste the content in sadcasm.js in new line.
   copy the url of sadcasm and replace it by previous url.
-  Restart the browser
   
 
  * */
